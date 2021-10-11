fbcf77839f62ef92763b42b6548769a863def4b5
noppakraw-c <noppakraw.c@codemonday.com>
2021-10-11 21:35:39 +0700

    add Compiling-TypeScript

---
+++ Compiling-TypeScript/index.js
@@ -0,0 +1,11 @@
+var name = 'game';
+var age = 25;
+var sex = 'man';
+var data = {
+    name: name,
+    age: age,
+    sex: sex
+};
+var profile = [];
+var countArray = profile.push(data);
+console.log(profile);
---
+++ Compiling-TypeScript/index.ts
@@ -0,0 +1,15 @@
+const name = 'game';
+const age = 25;
+const sex = 'man';
+
+const data = {
+  name,
+  age,
+  sex,
+}
+
+const profile = [];
+
+const countArray = profile.push(data);
+
+console.log(profile);
